61bf0d24608037081d9beb56a0c6f1a4ea693c53
nuray-alam <siddik4x@gmail.com>
2021-11-09 22:05:34 +0600

    final push

--- src/Pages/Dashboard/Appointments/Appointments.js
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -13,7 +13,7 @@ const Appointments = ({ date }) => {
     const [appointments, setAppointments] = useState([]);
     useEffect(() => {
 
-        const url = `https://serene-river-83100.herokuapp.com/appointments?email=${user.email}&date=${date}`
+        const url = `https://serene-river-83100.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`
         fetch(url, {
             headers: {
                 'authorization': `Bearer ${token}`
